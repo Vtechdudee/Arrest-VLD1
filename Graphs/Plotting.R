a2908f13da5d8b85a2a6cefb2a454798d46510fe
Vtechdudee <fanyacheng@gmail.com>
2017-03-18 16:18:39 -0400

    I think we should reset the format of time.

--- Graphs/Plotting.R
+++ Graphs/Plotting.R
@@ -1,1 +1,4 @@
-PlotByTime<-function(time,count){}
+PlotByTime<-function(time,count){ plot(count~time,
+                                  xlab="Time",ylab="Counts of Council Districts Having Crime",
+                                  main="The Spread of Crime",type="l",lwd=3)}
+
