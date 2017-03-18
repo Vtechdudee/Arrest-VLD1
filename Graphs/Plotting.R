c31edbf7613f80b96b2ee70c9d20ed198b913110
Vtechdudee <fanyacheng@gmail.com>
2017-03-18 16:30:15 -0400

    The graph is kind of weird.

--- Graphs/Plotting.R
+++ Graphs/Plotting.R
@@ -1,4 +1,4 @@
 PlotByTime<-function(time,count){ plot(count~time,
                                   xlab="Time",ylab="Counts of Council Districts Having Crime",
-                                  main="The Spread of Crime",type="l",lwd=3)}
+                                  main="The Spread of Crime",type="l",lwd=1)}
 
