284a8422929e531a2e9d06c8e2d5712982071583
Vtechdudee <fanyacheng@gmail.com>
2017-03-18 15:33:41 -0400

    AggregateByCase: Count crime cases on certain day UniqueAggregate: Count unique districts on certain day

--- Function/DataCleansing.R
+++ Function/DataCleansing.R
@@ -1,1 +1,3 @@
-aggregateByCase<-function(){}
+AggregateByCase<-function(group,x){tapply(x,group,length)}
+lenunique<-function(x){length(unique(x))}
+UniqueAggregate<-function(group,x){tapply(x,group,lenunique)}
